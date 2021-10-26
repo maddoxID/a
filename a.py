f29f1d5455ebf36846cdf6c9c267231fce570da0
maddoxID <93085520+maddoxID@users.noreply.github.com>
2021-10-26 21:38:59 +0700

    Update a.py

--- a.py
+++ a.py
@@ -1,10 +1,13 @@
+import sys
+
 print("halo")
-print()
-print()
+print
+print
 
 Username=raw_input("Username: ")
 Password=raw_input("Password: ")
 if(Username=='arimengen' and Password=='arimengen12'):
     print 'login..'
 else:
+    sys.os(clear)
     print 'Password anda Salah'
