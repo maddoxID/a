8a3d5691d275f23881ccdd71899509ae638795f9
maddoxID <93085520+maddoxID@users.noreply.github.com>
2021-10-27 14:45:30 +0700

    Update a.py

--- a.py
+++ a.py
@@ -1,5 +1,8 @@
+
 import os
 import sys
+import random
+import time
 
 print("halo")
 print
@@ -8,9 +11,21 @@ print
 Username=raw_input("Username: ")
 Password=raw_input("Password: ")
 if(Username=='arimengen' and Password=='arimengen12'):
+    print
     print 'login..'
+    os.system("clear")
+
+    def ketik(s):
+        sys.stdout.write(c)
+        sys.stdout.flush()
+
+        time.sleep(random.random() * 0.1)
+
+ketik("halo, Terimakah Telah Menggunakan Skrip Arimengen :)\n Jangan Lupa Subscribe Channel Gua!!")
+
+        
+    os.system("sudo apt-get install cmatrix")
+    os.system("cmatirx")
 else:
     print 'Password anda Salah'
     os.system("clear")
-
-    
